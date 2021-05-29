7b0282155e0ee96654507ab90e59eda6e7b6b6ea
rahulkudum <rahulkudum@gmail.com>
2021-05-29 22:48:36 +0530

    Updates

---
+++ precache-manifest.b9b592b79dc04e823ebca99816c672b1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b964e9b06b1c243a69b8",
+    "url": "/my-app/static/css/main.2cc4a1ca.chunk.css"
+  },
+  {
+    "revision": "b964e9b06b1c243a69b8",
+    "url": "/my-app/static/js/main.67321ca9.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "4c58431a90a72717388b",
+    "url": "/my-app/static/css/2.b218e830.chunk.css"
+  },
+  {
+    "revision": "4c58431a90a72717388b",
+    "url": "/my-app/static/js/2.4f1a0d34.chunk.js"
+  },
+  {
+    "revision": "3c9dcf46183f96f9644f67e10543294f",
+    "url": "/my-app/static/media/logo.3c9dcf46.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/my-app/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/my-app/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/my-app/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "780b81832d20bb094169f33dfac0ed9b",
+    "url": "/my-app/index.html"
+  }
+];
